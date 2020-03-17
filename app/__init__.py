509841381fd540da42d9e9ef2ddb365021320b67
jaey p <jp.inseoul@gmail.com>
2020-03-17 14:24:12 -0400

    initial commit

---
+++ app/__init__.py
@@ -0,0 +1,25 @@
+from flask import Flask
+from flask_cors import CORS
+from . import settings
+
+def create_app():
+    """
+    Construct the core application.
+    """
+
+    # Create flask app with CORS enabled.
+    app = Flask(__name__)
+    CORS(app)
+
+    # Set app config from settings.
+    app.config.from_pyfile('settings.py');
+
+    # Push an application context manually (explicitly)
+    with app.app_context():
+        # Initialization
+
+        # Import routes.
+        from . import routes
+
+    # Return created app.
+    return app
